620b90cf132129e5552ef32efe155bc990b94118
mbnahmadi <mbn.ahmadi78@gmail.com>
2025-03-12 20:31:01 +0330

    add report html

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def main_reports(request):
+    '''show main reports page'''
+    return render(request, 'reports/main-reports.html')
